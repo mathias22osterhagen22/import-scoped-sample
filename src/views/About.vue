f2292adc22bb6acc25bb6408033727cab0af519b
mathias22osterhagen22 <mathias.osterhagen@protonmail.com>
2020-02-06 16:43:08 +0100

    Example

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is the display page:</h1>
+    <img id="screen" src="../assets/example.jpg" />
   </div>
 </template>
+
+<style lang="css" scoped>
+@import "~viewerjs/dist/viewer.css";
+</style>
+
+<script>
+import Viewer from "viewerjs";
+
+export default {
+  mounted() {
+    console.log('About.vue is mounted');
+    /*let viewer = */new Viewer(this.$el.querySelector("#screen"), {
+      
+      viewed() {
+        console.log("hellow from viewer");
+      }
+    });
+  }
+};
+</script>
